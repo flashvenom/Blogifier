d16ab1a8bc8aca3eda4fdf2c52b3adbcc927ab75
flashvenom <flashvenomdesign@gmail.com>
2019-04-25 19:42:42 -0700

    homepage polish and about page foundation

---
+++ src/App/Controllers/AboutController.cs
@@ -0,0 +1,42 @@
+﻿using Core.Data;
+using Core.Data.Models;
+using Core.Helpers;
+using Core.Services;
+using Markdig;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace App.Controllers
+{
+    public class AboutController : Controller
+    {
+        IDataService _db;
+        IFeedService _ss;
+        SignInManager<AppUser> _sm;
+        private readonly ICompositeViewEngine _viewEngine;
+        static readonly string _listView = "~/Views/Themes/{0}/List.cshtml";
+
+        public AboutController(IDataService db, IFeedService ss, SignInManager<AppUser> sm, ICompositeViewEngine viewEngine)
+        {
+            _db = db;
+            _ss = ss;
+            _sm = sm;
+            _viewEngine = viewEngine;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var blog = await _db.CustomFields.GetBlogSettings();
+            var model = new AboutPage();
+            model.Blog = blog;
+            model.Title = "About Metro Insurance Services";
+            return View(model);
+        }   
+    }
+}
---
+++ src/Core/Data/Models/AboutModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Data.Models
+{
+    public class AboutPage
+    {
+        public BlogItem Blog { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Categories { get; set; }
+        public string Cover { get; set; }
+        public int PostViews { get; set; }
+        public double Rating { get; set; }
+    }
+    
+}
